1af1210966a2df300270e1ff25a4febdf0600127
Mario T. Lanza <mlanza@doesideas.com>
2022-05-07 11:17:56 -0400

    dev

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import {init, bank, roll, take, snatch} from "./pickomino.js";
+import {init, bank, roll, claim, steal} from "./pickomino.js";
 import _ from "./lib/@atomic/core.js";
 import $ from "./lib/@atomic/reactives.js";
 
 const $state = $.cell(init(["Mario", "Ava", "Zoe"]));
-Object.assign(window, {_, $}, {$state}, {bank, roll, take, snatch});
+Object.assign(window, {_, $}, {$state}, {bank, roll, claim, steal});
--- pickomino.js
+++ pickomino.js
@@ -4,7 +4,7 @@ export function init(names){
   const up = 0;
   const status = "ready";
   const players = _.mapa(function(name){
-    return {player: name, stack: []};
+    return {name, stack: []};
   }, names);
   const rolled = [];
   const banked = [];
@@ -19,62 +19,95 @@ export function rollDice(n){
   return _.sort(_.repeatedly(n, _.partial(_.randInt, 6)));
 }
 
+export function bankable(banked, rolled){
+  return _.chain(rolled, _.remove(_.includes(banked, _), _), _.unique, _.seq);
+}
+
+export const hasWorm = _.includes(_, 0);
+
+export function fail(state){
+  const {up, tiles} = state;
+  const tile = _.chain(state, _.getIn(_, ["players", up, "stack"]), _.first);
+  if (tile) {
+    const ref = _.count(_.takeWhile(function(t){
+      return tile.value < t.value;
+    }, tiles));
+    return _.chain(state,
+      _.updateIn(_, ["players", up, "stack"], _.rest),
+      _.update(_, "tiles", _.pipe(
+        _.before(_, ref, tile),
+        _.butlast)),
+        finish);
+  } else {
+    return finish(state);
+  }
+}
+
 export function roll(state){
-  const {banked, rolled} = state;
-  return {...state, status: "rolled", rolled: rollDice(banked.length ? rolled.length : 8)};
+  if (_.includes(["ready", "banked"], _.get(state, "status"))) {
+    const {banked, rolled} = state;
+    const dice = rollDice(_.count(banked) ? _.count(rolled) : 8);
+    const status = _.count(rolled) === 0 || bankable(banked, dice) ? "success" : "failure";
+    return _.chain({...state, status, rolled: dice}, status === "success" ? _.identity : fail);
+  } else {
+    return state;
+  }
 }
 
 export function bank(n){
   return function(state){
     const {banked, rolled, status} = state;
     const {"true": claimed, "false": unclaimed} = _.groupBy(_.and(_.eq(n, _), _.complement(_.includes(banked, _))), rolled);
-    return _.count(claimed) && status === "rolled" ? {...state, status: "banked", rolled: unclaimed, banked: _.sort(_.concat(claimed, banked))} : state;
+    const _banked = _.sort(_.concat(claimed, banked));
+    return _.count(claimed) && status === "success" ? _.chain({...state, status: "banked", rolled: unclaimed, banked: _banked}, hasWorm(_banked) ? _.identity : roll) : state;
   }
 }
 
-function worth(banked){
+export function worth(banked){
   return _.sum(_.map(function(n){
     return n ? n : 5;
   }, banked));
 }
 
-function snatchable(players, up, v){
-  return _.keepIndexed(function(idx, {stack}){
-    const {worms, value} = _.first(stack);
+export function exposed(players, up, v){
+  return _.first(_.keepIndexed(function(idx, {stack}){
+    const {worms, value} = _.first(stack) || {};
     return value === v && idx !== up ? idx : null;
-  }, players);
+  }, players));
 }
 
-export function take(v){
+export function finish(state){
+  const {up, players, tiles} = state;
+  const n = up + 1;
+  const status = _.count(tiles) ? "ready" : "over";
+  return {...state, rolled: [], banked: [], status, up: n > _.count(players) ? 0 : n};
+}
+
+export function claim(v){
   return function(state){
-    const {banked, tiles} = state;
+    const {up, banked, tiles} = state;
     const {"true": took, "false": left} = _.groupBy(function({worms, value}){
       return value === v;
     }, tiles);
-    return v <= worth(banked) && _.count(took) ?
+    return v <= worth(banked) && _.count(took) && hasWorm(banked) ?
       _.chain(state,
         _.updateIn(_, ["players", up, "stack"], _.prepend(_, _.first(took))),
         _.assocIn(_, ["tiles"], left),
-        finish) :
+        finish,
+        roll) :
       state;
   }
 }
 
-function finish(state){
-  const {up, players} = state;
-  const n = up + 1;
-  return {...state, status: "ready", up: n > _.count(players) ? 0 : n};
-}
-
-export function snatch(v){
+export function steal(v){
   return function(state){
     const {banked, players, up} = state;
-    const at = snatchable(players, up, v);
+    const at = exposed(players, up, v);
     const tile = _.first(_.getIn(players, [at, "stack"]));
-    return v === worth(banked) && who ?
+    return v === worth(banked) && at !== null ?
       _.chain(state,
-        _.updateIn(_, ["players", up, "stack"], _.prepend(_, tile)),
         _.updateIn(_, ["players", at, "stack"], _.comp(_.toArray, _.rest)),
+        _.updateIn(_, ["players", up, "stack"], _.prepend(_, tile)),
         finish) :
       state;
   }
